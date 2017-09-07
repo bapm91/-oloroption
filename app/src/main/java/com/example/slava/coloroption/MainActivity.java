c31217533b83fe6cfb219e8739ef74103a1fee33
bapm91 <Kulynych.v.a@gmail.com>
2017-09-07 12:20:17 +0300

    problem with api 19

--- app/src/main/java/com/example/slava/coloroption/MainActivity.java
+++ app/src/main/java/com/example/slava/coloroption/MainActivity.java
@@ -1,13 +1,109 @@
 package com.example.slava.coloroption;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private TextView textRed;
+    private TextView textGreen;
+    private TextView textBlue;
+    private int valueRed;
+    private int valueGreen;
+    private int valueBlue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        valueRed = 128;
+        valueGreen = 128;
+        valueBlue = 128;
+
+        imageView = (ImageView) findViewById(R.id.color_image);
+        textRed = (TextView) findViewById(R.id.value_red);
+        textGreen = (TextView) findViewById(R.id.value_green);
+        textBlue = (TextView) findViewById(R.id.value_blue);
+
+        SeekBar seekRed = (SeekBar) findViewById(R.id.seekBarRed);
+        SeekBar seekGreen = (SeekBar) findViewById(R.id.seekBarGreen);
+        SeekBar seekBlue = (SeekBar) findViewById(R.id.seekBarBlue);
+
+        seekRed.setOnSeekBarChangeListener(seekBarListenerRed);
+        seekGreen.setOnSeekBarChangeListener(seekBarListenerGreen);
+        seekBlue.setOnSeekBarChangeListener(seekBarListenerBlue);
+
     }
+
+    private final SeekBar.OnSeekBarChangeListener seekBarListenerRed =
+            new SeekBar.OnSeekBarChangeListener() {
+                // Обновление процента чаевых и вызов calculate
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress,
+                                              boolean fromUser) {
+                    valueRed = progress;
+                    textRed.setText(String.valueOf(valueRed));
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        imageView.setBackgroundColor(Color.argb(255, valueRed, valueGreen, valueBlue));
+                    }
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                }
+            };
+
+    private final SeekBar.OnSeekBarChangeListener seekBarListenerGreen =
+            new SeekBar.OnSeekBarChangeListener() {
+                // Обновление процента чаевых и вызов calculate
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress,
+                                              boolean fromUser) {
+                    valueGreen = progress;
+                    textGreen.setText(String.valueOf(valueGreen));
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        imageView.setBackgroundColor(Color.argb(255, valueRed, valueGreen, valueBlue));
+                    }
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                }
+            };
+
+    private final SeekBar.OnSeekBarChangeListener seekBarListenerBlue =
+            new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress,
+                                              boolean fromUser) {
+                    valueBlue = progress;
+                    textBlue.setText(String.valueOf(valueBlue));
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        imageView.setBackgroundColor(Color.argb(255, valueRed, valueGreen, valueBlue));
+                    }
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                }
+            };
 }
